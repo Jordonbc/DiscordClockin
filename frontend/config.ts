61678415c9447ab0c5f596e606ed4fb40bb0422d
Jordon <jordon@bbgames.dev>
2025-10-01 09:09:51 +0100

    Update config.ts

--- frontend/config.ts
+++ frontend/config.ts
@@ -10,7 +10,7 @@ const runtimeConfig: FrontendConfig = {
   apiBaseUrl: "http://127.0.0.1:8080",
   guildId: "1016787224391585823",
   discordClientId: "1418388811016310805",
-  discordRedirectUri: "http://localhost:3000",
+  discordRedirectUri: "http://localhost:5173",
   discordScopes: ["identify"],
   adminUserIds: ["104546226938073088"],
 };
